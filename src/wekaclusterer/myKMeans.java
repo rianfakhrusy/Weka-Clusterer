f69b1d01179120c99f5099c4f58dd86dad88efad
atianadya <theaolivia.mail@gmail.com>
2016-11-22 21:06:21 +0700

    myKMeans cleaning complete

--- src/wekaclusterer/myKMeans.java
+++ src/wekaclusterer/myKMeans.java
@@ -10,27 +10,27 @@ import weka.filters.unsupervised.attribute.ReplaceMissingValues;
 public class myKMeans extends RandomizableClusterer {
 
   /**
-   * training instances
+   * Instansi dari training
    */
   private Instances m_instances;
 
   /**
-   * replace missing values in training instances
+   * menggantikan missing values
    */
   private ReplaceMissingValues m_ReplaceMissingFilter;
 
   /**
-   * number of clusters to generate
+   * banyak cluster untuk digenerate
    */
   private int m_NumClusters = 2;
 
   /**
-   * holds the cluster centroids
+   * menangani centroid
    */
   private Instances m_ClusterCentroids;
 
   /**
-   * temporary variable holding cluster assignments while iterating
+   * variable cluster temporary
    */
   private int [] m_ClusterAssignments;
 
@@ -40,26 +40,20 @@ public class myKMeans extends RandomizableClusterer {
   private int m_Seed = 10;
 
   /**
-   * attribute min values
+   * nilai minimum dari atribut
    */
   private double [] m_Min;
   
   /**
-   * attribute max values
+   * nilai maksimum dari atribut
    */
   private double [] m_Max;
   
-  /**
-   * Generates a clusterer. Has to initialize all fields of the clusterer
-   *
-   * @param data set of instances serving as training data 
-   * @exception Exception if the clusterer has not been 
-   * generated successfully
-   */
+
   public void buildClusterer(Instances data) throws Exception {
     
     if (data.checkForStringAttributes()) {
-      throw  new Exception("Can't handle string attributes!");
+      throw  new Exception("Attribut tidak dapat ditangani :(");
     }
 
     m_ReplaceMissingFilter = new ReplaceMissingValues();
@@ -99,7 +93,7 @@ public class myKMeans extends RandomizableClusterer {
       }
       
       Instances [] tempI = new Instances[m_NumClusters];
-      // update centroids
+      // update centroid
       m_ClusterCentroids = new Instances(m_instances, m_NumClusters);
       for (int i = 0; i < m_NumClusters; i++) {
 	tempI[i] = new Instances(m_instances, 0);
@@ -117,12 +111,7 @@ public class myKMeans extends RandomizableClusterer {
     }
   }
 
-  /**
-   * clusters an instance that has been through the filters
-   *
-   * @param instance the instance to assign a cluster to
-   * @return a cluster number
-   */
+
   private int clusterProcessedInstance(Instance instance) {
     double minDist = Integer.MAX_VALUE;
     int bestCluster = 0;
@@ -136,15 +125,7 @@ public class myKMeans extends RandomizableClusterer {
     return bestCluster;
   }
 
-  /**
-   * Classifies a given instance.
-   *
-   * @param instance the instance to be assigned to a cluster
-   * @return the number of the assigned cluster as an interger
-   * if the class is enumerated, otherwise the predicted value
-   * @exception Exception if instance could not be classified
-   * successfully
-   */
+
   public int clusterInstance(Instance instance) throws Exception {
     m_ReplaceMissingFilter.input(instance);
     m_ReplaceMissingFilter.batchFinished();
@@ -153,13 +134,7 @@ public class myKMeans extends RandomizableClusterer {
     return clusterProcessedInstance(inst);
   }
 
-  /**
-   * Calculates the distance between two instances
-   *
-   * @param test the first instance
-   * @param train the second instance
-   * @return the distance between the two given instances, between 0 and 1
-   */          
+      
   private double distance(Instance first, Instance second) {  
 
     double distance = 0;
@@ -204,16 +179,12 @@ public class myKMeans extends RandomizableClusterer {
     return Math.sqrt(distance / m_instances.numAttributes());
   }
 
-  /**
-   * Computes the difference between two given attribute
-   * values.
-   */
   private double difference(int index, double val1, double val2) {
 
     switch (m_instances.attribute(index).type()) {
     case Attribute.NOMINAL:
       
-      // If attribute is nominal
+      // kalau atribut nominal
       if (Instance.isMissingValue(val1) || 
 	  Instance.isMissingValue(val2) ||
 	  ((int)val1 != (int)val2)) {
@@ -223,7 +194,7 @@ public class myKMeans extends RandomizableClusterer {
       }
     case Attribute.NUMERIC:
 
-      // If attribute is numeric
+      // kalau atribut numerik
       if (Instance.isMissingValue(val1) || 
 	  Instance.isMissingValue(val2)) {
 	if (Instance.isMissingValue(val1) && 
@@ -249,12 +220,7 @@ public class myKMeans extends RandomizableClusterer {
     }
   }
 
-  /**
-   * Normalizes a given value of a numeric attribute.
-   *
-   * @param x the value to be normalized
-   * @param i the attribute's index
-   */
+
   private double norm(double x, int i) {
 
     if (Double.isNaN(m_Min[i]) || Utils.eq(m_Max[i],m_Min[i])) {
@@ -264,12 +230,7 @@ public class myKMeans extends RandomizableClusterer {
     }
   }
 
-  /**
-   * Updates the minimum and maximum values for all the attributes
-   * based on a new instance.
-   *
-   * @param instance the new instance
-   */
+
   private void updateMinMax(Instance instance) {  
 
     for (int j = 0;j < m_instances.numAttributes(); j++) {
@@ -290,78 +251,44 @@ public class myKMeans extends RandomizableClusterer {
     }
   }
   
-  /**
-   * Returns the number of clusters.
-   *
-   * @return the number of clusters generated for a training dataset.
-   * @exception Exception if number of clusters could not be returned
-   * successfully
-   */
+
   public int numberOfClusters() throws Exception {
     return m_NumClusters;
   }
   
-  /**
-   * Returns the tip text for this property
-   * @return tip text for this property suitable for
-   * displaying in the explorer/experimenter gui
-   */
+
   public String numClustersTipText() {
     return "set number of clusters";
   }
 
-  /**
-   * set the number of clusters to generate
-   *
-   * @param n the number of clusters to generate
-   */
+
   public void setNumClusters(int n) {
     m_NumClusters = n;
   }
 
-  /**
-   * gets the number of clusters to generate
-   *
-   * @return the number of clusters to generate
-   */
+
   public int getNumClusters() {
     return m_NumClusters;
   }
     
-  /**
-   * Returns the tip text for this property
-   * @return tip text for this property suitable for
-   * displaying in the explorer/experimenter gui
-   */
+
   public String seedTipText() {
     return "random number seed";
   }
 
 
-  /**
-   * Set the random number seed
-   *
-   * @param s the seed
-   */
+
   public void setSeed (int s) {
     m_Seed = s;
   }
 
 
-  /**
-   * Get the random number seed
-   *
-   * @return the seed
-   */
+
   public int getSeed () {
     return  m_Seed;
   }
 
-  /**
-   * return a string describing this clusterer
-   *
-   * @return a description of the clusterer as a string
-   */
+
   public String toString() {
     StringBuffer temp = new StringBuffer();
 
