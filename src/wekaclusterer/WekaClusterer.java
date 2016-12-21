3ee9f8ca2c95a62a87dd9e145a0a1ee62325b40b
Rian Fakhrusy <staroushi38@gmail.com>
2016-12-21 20:58:16 +0700

    Final

--- src/wekaclusterer/WekaClusterer.java
+++ src/wekaclusterer/WekaClusterer.java
@@ -7,64 +7,87 @@ import weka.core.converters.ConverterUtils;
 import weka.filters.unsupervised.attribute.Remove;
 
 public class WekaClusterer {
-
+    private static boolean KELUAR = false;
+    private static String[] options = new String[4];
+    private static int cluster;
+    
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        //load data
-        String filedataset = "iris.arff"; //perlu input nama file harusnya
-        Instances dataset = new ConverterUtils.DataSource(filedataset).getDataSet();
-        if(dataset.classIndex() == -1)
-            dataset.setClassIndex(dataset.numAttributes() - 1);
-        dataset.deleteStringAttributes();
-        
-        //remove class attribute
-        Instances dataClusterer = null;
-        Remove filter = new Remove();
-        filter.setAttributeIndices("" + (dataset.classIndex() + 1));
-        try {
-            filter.setInputFormat(dataset);
-            dataClusterer = filter.useFilter(dataset, filter);
-        } catch (Exception e1) {
-            e1.printStackTrace();
-            return;
+        while(!KELUAR){
+            Scanner sc = new Scanner(System.in);
+            //System.out.println("Masukkan nama data set : ");
+            String filedataset = "cri.arff";
+            Instances dataset = new ConverterUtils.DataSource(filedataset).getDataSet();
+            if(dataset.classIndex() == -1)
+                dataset.setClassIndex(dataset.numAttributes() - 1);
+            dataset.deleteStringAttributes();
+
+            //remove class attribute
+            Instances dataClusterer = null;
+            Remove filter = new Remove();
+            filter.setAttributeIndices("" + (dataset.classIndex() + 1));
+            try {
+                filter.setInputFormat(dataset);
+                dataClusterer = filter.useFilter(dataset, filter);
+            } catch (Exception e1) {
+                e1.printStackTrace();
+                return;
+            }
+
+            //pilih algoritma
+            System.out.println("Masukkan algoritma clustering");
+            System.out.println("1. Simple K-Means");
+            System.out.println("2. Agglomerative Clustering : Single Link");
+            System.out.println("3. Agglomerative Clustering : Complete Link");
+            System.out.println("4. Keluar");
+            System.out.print("Input: ");  
+            int input = sc.nextInt(); sc.nextLine();   
+            
+            ClusterEvaluation eval = new ClusterEvaluation();
+            switch(input){
+                case 1 : {
+                            System.out.print("Tentukan jumlah cluster: ");
+                            cluster = sc.nextInt();
+                            myKMeans clusterer = new myKMeans();   // new instance of clusterer
+                            clusterer.setNumClusters(cluster);
+                            clusterer.buildClusterer(dataClusterer);    // build the clusterer
+                            eval.setClusterer(clusterer); // the cluster to evaluate
+                            eval.evaluateClusterer(dataset); // data to evaluate the clusterer on
+                            System.out.println(eval.clusterResultsToString());  //output the result
+                            break;
+                        }
+                case 2 : {
+                            System.out.print("Tentukan jumlah cluster: ");
+                            cluster = sc.nextInt();
+                            myAgnes clusterer = new myAgnes();
+                            clusterer.setNumClusters(cluster);
+                            clusterer.setLinkType(0);
+                            clusterer.buildClusterer(dataClusterer);    // build the clusterer
+                            eval.setClusterer(clusterer); // the cluster to evaluate
+                            eval.evaluateClusterer(dataset); // data to evaluate the clusterer on
+                            System.out.println(eval.clusterResultsToString());  //output the result
+                            break;
+                        }
+                case 3 : {
+                            System.out.print("Tentukan jumlah cluster: ");
+                            cluster = sc.nextInt();
+                            myAgnes clusterer = new myAgnes();
+                            clusterer.setNumClusters(cluster);
+                            clusterer.setLinkType(1);
+                            clusterer.buildClusterer(dataClusterer);    // build the clusterer
+                            eval.setClusterer(clusterer); // the cluster to evaluate
+                            eval.evaluateClusterer(dataset); // data to evaluate the clusterer on
+                            System.out.println(eval.clusterResultsToString());  //output the result
+                            break;
+                        }
+                case 4 : {
+                            KELUAR = true;
+                            break;
+                        }
+                default :{
+                            System.out.println("Anda memberi input yang salah !");
+                            break;
+                        }
+            }
         }
-        
-        //pilih algoritma
-        System.out.println("Masukkan algoritma clustering");
-        System.out.println("1. Simple K-Means");
-        System.out.println("2. Agglomerative Clustering : Single Link");
-        System.out.println("3. Agglomerative Clustering : Complete Link");
-        System.out.print("Input: ");  
-        int input = sc.nextInt(); sc.nextLine();
-        
-        //tentukan opsi algoritma yang dipiilih
-        String[] options = new String[4];
-        options[0] = "-N"; // number of cluster
-        System.out.print("Tentukan jumlah cluster: "); //3 cluster untuk dataset iris
-        options[1] = sc.nextInt() + "";
-        options[2] = "-L"; // link type (SINGLE-LINK/COMPLETE-LINK)
-        
-        ClusterEvaluation eval = new ClusterEvaluation();
-        if (input==1){ //build K-Means clusterer
-            myKMeans clusterer = new myKMeans();   // new instance of clusterer
-            clusterer.setNumClusters(Integer.parseInt(options[1]));
-            clusterer.buildClusterer(dataClusterer);    // build the clusterer
-            eval.setClusterer(clusterer); // the cluster to evaluate
-        } else if (input==2) {
-            myAgnes clusterer = new myAgnes();
-            options[3] = "SINGLE";
-            clusterer.setOptions(options);
-            clusterer.buildClusterer(dataClusterer);    // build the clusterer
-            eval.setClusterer(clusterer); // the cluster to evaluate
-        } else if (input==3) {
-            myAgnes clusterer = new myAgnes();
-            options[3] = "COMPLETE";
-            clusterer.setOptions(options);
-            clusterer.buildClusterer(dataClusterer);    // build the clusterer
-            eval.setClusterer(clusterer); // t8he cluster to evaluate
-        }
-        
-        eval.evaluateClusterer(dataset); // data to evaluate the clusterer on
-        System.out.println(eval.clusterResultsToString());  //output the result
     }
 }
--- src/wekaclusterer/myKMeans.java
+++ src/wekaclusterer/myKMeans.java
@@ -1,311 +1,272 @@
 package wekaclusterer;
 
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.Vector;
 import weka.clusterers.RandomizableClusterer;
 import weka.core.*;
+import weka.core.Capabilities.Capability;
 import weka.filters.Filter;
 import weka.filters.unsupervised.attribute.ReplaceMissingValues;
 
 public class myKMeans extends RandomizableClusterer {
-
-  /**
-   * Instansi dari training
-   */
-  private Instances m_instances;
-
-  /**
-   * menggantikan missing values
-   */
-  private ReplaceMissingValues m_ReplaceMissingFilter;
-
-  /**
-   * banyak cluster untuk digenerate
-   */
-  private int m_NumClusters = 2;
-
-  /**
-   * menangani centroid
-   */
-  private Instances m_ClusterCentroids;
-
-  /**
-   * variable cluster temporary
-   */
-  private int [] m_ClusterAssignments;
-
-  /**
-   * random seed
-   */
-  private int m_Seed = 10;
-
-  /**
-   * nilai minimum dari atribut
-   */
-  private double [] m_Min;
-  
-  /**
-   * nilai maksimum dari atribut
-   */
-  private double [] m_Max;
-  
-
-  public void buildClusterer(Instances data) throws Exception {
+    private Instances m_instances; //training data
+    private ReplaceMissingValues m_ReplaceMissingFilter; //for replacement of missing value
+    private int m_NumClusters; //number of clusters
+    private Instances m_ClusterCentroids; //clusters centroid
+    private int m_Seed; //randomization seed
+    private ArrayList<ArrayList<Integer>> nClusterID; //storing cluster ID
+    private int m_Iterations; //number of iterations before convergence
+    private double[] m_Min; //store minimum values for all attributes
+    private double[] m_Max; //store maximum values for all attributes
+    protected double[] m_squaredErrors;
     
-    if (data.checkForStringAttributes()) {
-      throw  new Exception("Attribut tidak dapat ditangani :(");
-    }
-
-    m_ReplaceMissingFilter = new ReplaceMissingValues();
-    m_ReplaceMissingFilter.setInputFormat(data);
-    m_instances = Filter.useFilter(data, m_ReplaceMissingFilter);
-
-    m_Min = new double [m_instances.numAttributes()];
-    m_Max = new double [m_instances.numAttributes()];
-    for (int i = 0; i < m_instances.numAttributes(); i++) {
-      m_Min[i] = m_Max[i] = Double.NaN;
-    }
-
-    for (int i = 0; i < m_instances.numInstances(); i++) {
-      updateMinMax(m_instances.instance(i));
+    //constructor
+    public myKMeans() {
+        this.nClusterID = new ArrayList<>();
+        this.m_NumClusters = 2;
+        this.m_Seed = 10;
+        this.m_Iterations = 0;
     }
     
-    m_ClusterCentroids = new Instances(m_instances, m_NumClusters);
-    m_ClusterAssignments = new int [m_instances.numInstances()];
-
-    Random RandomO = new Random(m_Seed);
-
-    for (int i = 0; i < m_NumClusters; i++) {
-      int instIndex = Math.abs(RandomO.nextInt()) % m_instances.numInstances();
-      m_ClusterCentroids.add(m_instances.instance(instIndex));
-    }
-
-    boolean converged = false;
-    while (!converged) {
-      converged = true;
-      for (int i = 0; i < m_instances.numInstances(); i++) {
-	Instance toCluster = m_instances.instance(i);
-	int newC = clusterProcessedInstance(toCluster);
-	if (newC != m_ClusterAssignments[i]) {
-	  converged = false;
-	}
-	m_ClusterAssignments[i] = newC;
-      }
-      
-      Instances [] tempI = new Instances[m_NumClusters];
-      // update centroid
-      m_ClusterCentroids = new Instances(m_instances, m_NumClusters);
-      for (int i = 0; i < m_NumClusters; i++) {
-	tempI[i] = new Instances(m_instances, 0);
-      }
-      for (int i = 0; i < m_instances.numInstances(); i++) {
-	tempI[m_ClusterAssignments[i]].add(m_instances.instance(i));
-      }
-      for (int i = 0; i < m_NumClusters; i++) {
-	double [] vals = new double[m_instances.numAttributes()];
-	for (int j = 0; j < m_instances.numAttributes(); j++) {
-	  vals[j] = tempI[i].meanOrMode(j);
-	}
-	m_ClusterCentroids.add(new Instance(1.0, vals));
-      }
-    }
-  }
-
-
-  private int clusterProcessedInstance(Instance instance) {
-    double minDist = Integer.MAX_VALUE;
-    int bestCluster = 0;
-    for (int i = 0; i < m_NumClusters; i++) {
-      double dist = distance(instance, m_ClusterCentroids.instance(i));
-      if (dist < minDist) {
-	minDist = dist;
-	bestCluster = i;
-      }
-    }
-    return bestCluster;
-  }
-
-
-  public int clusterInstance(Instance instance) throws Exception {
-    m_ReplaceMissingFilter.input(instance);
-    m_ReplaceMissingFilter.batchFinished();
-    Instance inst = m_ReplaceMissingFilter.output();
-
-    return clusterProcessedInstance(inst);
-  }
-
-      
-  private double distance(Instance first, Instance second) {  
-
-    double distance = 0;
-    int firstI, secondI;
-
-    for (int p1 = 0, p2 = 0; 
-	 p1 < first.numValues() || p2 < second.numValues();) {
-      if (p1 >= first.numValues()) {
-	firstI = m_instances.numAttributes();
-      } else {
-	firstI = first.index(p1); 
-      }
-      if (p2 >= second.numValues()) {
-	secondI = m_instances.numAttributes();
-      } else {
-	secondI = second.index(p2);
-      }
-      if (firstI == m_instances.classIndex()) {
-	p1++; continue;
-      } 
-      if (secondI == m_instances.classIndex()) {
-	p2++; continue;
-      } 
-      double diff;
-      if (firstI == secondI) {
-	diff = difference(firstI, 
-			  first.valueSparse(p1),
-			  second.valueSparse(p2));
-	p1++; p2++;
-      } else if (firstI > secondI) {
-	diff = difference(secondI, 
-			  0, second.valueSparse(p2));
-	p2++;
-      } else {
-	diff = difference(firstI, 
-			  first.valueSparse(p1), 0);
-	p1++;
-      }
-      distance += diff * diff;
+    //capability handler
+    @Override
+    public Capabilities getCapabilities() {
+        Capabilities result = super.getCapabilities();
+        result.disableAll();
+        //class
+        result.enable(Capability.NO_CLASS);
+        result.enable(Capability.MISSING_VALUES);
+        // attributes
+        result.enable(Capability.NOMINAL_ATTRIBUTES);
+        result.enable(Capability.NUMERIC_ATTRIBUTES);
+        return result;
     }
     
-    return Math.sqrt(distance / m_instances.numAttributes());
-  }
-
-  private double difference(int index, double val1, double val2) {
-
-    switch (m_instances.attribute(index).type()) {
-    case Attribute.NOMINAL:
-      
-      // kalau atribut nominal
-      if (Instance.isMissingValue(val1) || 
-	  Instance.isMissingValue(val2) ||
-	  ((int)val1 != (int)val2)) {
-	return 1;
-      } else {
-	return 0;
-      }
-    case Attribute.NUMERIC:
-
-      // kalau atribut numerik
-      if (Instance.isMissingValue(val1) || 
-	  Instance.isMissingValue(val2)) {
-	if (Instance.isMissingValue(val1) && 
-	    Instance.isMissingValue(val2)) {
-	  return 1;
-	} else {
-	  double diff;
-	  if (Instance.isMissingValue(val2)) {
-	    diff = norm(val1, index);
-	  } else {
-	    diff = norm(val2, index);
-	  }
-	  if (diff < 0.5) {
-	    diff = 1.0 - diff;
-	  }
-	  return diff;
-	}
-      } else {
-	return norm(val1, index) - norm(val2, index);
-      }
-    default:
-      return 0;
-    }
-  }
-
-
-  private double norm(double x, int i) {
-
-    if (Double.isNaN(m_Min[i]) || Utils.eq(m_Max[i],m_Min[i])) {
-      return 0;
-    } else {
-      return (x - m_Min[i]) / (m_Max[i] - m_Min[i]);
-    }
-  }
-
-
-  private void updateMinMax(Instance instance) {  
-
-    for (int j = 0;j < m_instances.numAttributes(); j++) {
-      if (!instance.isMissing(j)) {
-	if (Double.isNaN(m_Min[j])) {
-	  m_Min[j] = instance.value(j);
-	  m_Max[j] = instance.value(j);
-	} else {
-	  if (instance.value(j) < m_Min[j]) {
-	    m_Min[j] = instance.value(j);
-	  } else {
-	    if (instance.value(j) > m_Max[j]) {
-	      m_Max[j] = instance.value(j);
-	    }
-	  }
-	}
-      }
-    }
-  }
-  
-
-  public int numberOfClusters() throws Exception {
-    return m_NumClusters;
-  }
-  
-
-  public String numClustersTipText() {
-    return "set number of clusters";
-  }
-
-
-  public void setNumClusters(int n) {
-    m_NumClusters = n;
-  }
-
-
-  public int getNumClusters() {
-    return m_NumClusters;
-  }
+    @Override
+    public void buildClusterer(Instances data) throws Exception {
+        //replace instance with missing values
+        m_ReplaceMissingFilter = new ReplaceMissingValues();
+        m_ReplaceMissingFilter.setInputFormat(data);
+        m_instances = Filter.useFilter(data, m_ReplaceMissingFilter);
+         m_squaredErrors = new double[m_NumClusters];
+         
+        //initialize minimum and maximum value of attributes
+        m_Min = new double[m_instances.numAttributes()];
+        m_Max = new double[m_instances.numAttributes()];
+        for (int i = 0; i < m_instances.numAttributes(); i++)
+            m_Min[i] = m_Max[i] = Double.NaN;
+        
+        //updates minimum and maximum values for all attributes based on new instance
+        for (int i = 0; i < m_instances.numInstances(); i++){
+            for (int j = 0;j < m_instances.numAttributes(); j++){
+                Instance instance = m_instances.instance(i);
+                if (!instance.isMissing(j))
+                    if (Double.isNaN(m_Min[j])) {
+                        m_Min[j] = instance.value(j);
+                        m_Max[j] = instance.value(j);
+                    } else if (instance.value(j) < m_Min[j])
+                        m_Min[j] = instance.value(j);
+                    else if (instance.value(j) > m_Max[j])
+                        m_Max[j] = instance.value(j);
+            }
+        }
     
-
-  public String seedTipText() {
-    return "random number seed";
-  }
-
-
-
-  public void setSeed (int s) {
-    m_Seed = s;
-  }
-
-
-
-  public int getSeed () {
-    return  m_Seed;
-  }
-
-
-  public String toString() {
-    StringBuffer temp = new StringBuffer();
-
-    temp.append("\nkMeans\n======\n");
-
-    temp.append("\nCluster centroids:\n");
-    for (int i = 0; i < m_NumClusters; i++) {
-      temp.append("\nCluster "+i+"\n\t");
-      for (int j = 0; j < m_ClusterCentroids.numAttributes(); j++) {
-	if (m_ClusterCentroids.attribute(j).isNominal()) {
-	  temp.append(" "+m_ClusterCentroids.attribute(j).
-		      value((int)m_ClusterCentroids.instance(i).value(j)));
-	} else {
-	  temp.append(" "+m_ClusterCentroids.instance(i).value(j));
-	}
-      }
+        m_ClusterCentroids = new Instances(m_instances, m_NumClusters); //temp for centroids
+        int[] m_ClusterAssignments = new int[m_instances.numInstances()]; //temp variable for cluster assignment
+        
+        //select initial centroid at random
+        Random random = new Random(m_Seed);
+        for (int i = 0; i < m_NumClusters; i++) {
+            int instIndex = Math.abs(random.nextInt()) % m_instances.numInstances();
+            m_ClusterCentroids.add(m_instances.instance(instIndex));
+        }
+        
+        //begin iterations until converged
+        boolean converged = false;
+        while (!converged) {
+            m_squaredErrors = new double[m_NumClusters];
+            m_Iterations++; //iterations increment
+            converged = true;
+            //assign instance to a cluster
+            for (int i = 0; i < m_instances.numInstances(); i++) {
+                Instance toCluster = m_instances.instance(i);
+                int newC = clusterInstance(toCluster);
+                if (newC != m_ClusterAssignments[i])
+                    converged = false;
+                m_ClusterAssignments[i] = newC;
+            }
+            
+            Instances[] temp = new Instances[m_NumClusters];
+            m_ClusterCentroids = new Instances(m_instances, m_NumClusters); 
+            for (int i = 0; i < m_NumClusters; i++)
+                temp[i] = new Instances(m_instances, 0);
+            //adding instance to cluster
+            for (int i = 0; i < m_instances.numInstances(); i++)
+                temp[m_ClusterAssignments[i]].add(m_instances.instance(i));
+            //update centroids
+            for (int i = 0; i < m_NumClusters; i++) {
+                double[] vals = new double[m_instances.numAttributes()];
+                for (int j = 0; j < m_instances.numAttributes(); j++)
+                  vals[j] = temp[i].meanOrMode(j);
+                m_ClusterCentroids.add(new Instance(1.0, vals)); 
+            }
+        }
+        //record the member of clusters
+        for (int i = 0; i < numberOfClusters(); i++){
+            nClusterID.add(new ArrayList<>());
+        }
+        for (int i = 0; i < m_instances.numInstances(); i++){
+            nClusterID.get(m_ClusterAssignments[i]).add(i);
+        }
     }
-    return temp.toString();
-  }
+
+    //assign a given instance to a cluster
+    @Override
+    public int clusterInstance(Instance instance) throws Exception {
+        //replace missing value
+        m_ReplaceMissingFilter.input(instance);
+        m_ReplaceMissingFilter.batchFinished();
+        Instance inst = m_ReplaceMissingFilter.output();
+        //cluster instance
+        double minDist = Integer.MAX_VALUE; //minimum distance initialization
+        int bestCluster = 0; //default cluster
+        //search best cluster for an instance
+        for (int i = 0; i < m_NumClusters; i++) {
+            double dist = distance(inst, m_ClusterCentroids.instance(i));
+            if (dist < minDist) {
+                minDist = dist;
+                bestCluster = i;
+            }
+        }
+        minDist *= minDist;
+        m_squaredErrors[bestCluster] += minDist;
+        return bestCluster;
+    }
+
+    //calculates the distance between two instances
+    private double distance(Instance first, Instance second) {  
+        double distance = 0;
+        int firstI, secondI;
+
+        for (int p1 = 0, p2 = 0; p1 < first.numValues() || p2 < second.numValues();) {
+            firstI = (p1 >= first.numValues())? m_instances.numAttributes(): first.index(p1);
+            secondI = (p2 >= second.numValues())? m_instances.numAttributes(): second.index(p2); 
+            if (firstI == m_instances.classIndex())
+                p1++; 
+            else if (secondI == m_instances.classIndex())
+                p2++;
+            else {
+                double diff;
+                if (firstI == secondI) {
+                    diff = difference(firstI,first.valueSparse(p1),second.valueSparse(p2));
+                    p1++; 
+                    p2++;
+                } else if (firstI > secondI) {
+                    diff = difference(secondI,0, second.valueSparse(p2));
+                    p2++;
+                } else {
+                    diff = difference(firstI,first.valueSparse(p1), 0);
+                    p1++;
+                }
+                distance += diff * diff;
+            }
+        }
+
+        return Math.sqrt(distance / m_instances.numAttributes());
+    }
+
+    //count the distance between two attributes values
+    private double difference(int index, double val1, double val2) {
+        switch (m_instances.attribute(index).type()) {
+            case Attribute.NOMINAL: //if attribute is nominal, check if there is missing value
+                if (Instance.isMissingValue(val1) || Instance.isMissingValue(val2) || ((int)val1 != (int)val2))
+                    return 1;
+                else
+                    return 0;
+            case Attribute.NUMERIC: //if attribute is numeric, check if there is missing values and normalize
+                if (Instance.isMissingValue(val1) || Instance.isMissingValue(val2)) { 
+                    if (Instance.isMissingValue(val1) &&  Instance.isMissingValue(val2))
+                        return 1;
+                    else {
+                        double diff;
+                        if (Instance.isMissingValue(val2))
+                            diff = normalize(val1, index);
+                        else 
+                            diff = normalize(val2, index);
+                        if (diff < 0.5) 
+                            diff = 1.0 - diff;
+                        return diff;
+                    }
+                } else
+                    return normalize(val1, index) - normalize(val2, index);
+            default:
+                return 0;
+        }
+    }
+
+    //normalize numeric attribute.
+    private double normalize(double x, int i) {
+        if (Double.isNaN(m_Min[i]) || Utils.eq(m_Max[i],m_Min[i]))
+            return 0;
+        else
+            return (x - m_Min[i]) / (m_Max[i] - m_Min[i]);
+    }
+  
+    //return the number of clusters
+    @Override
+    public int numberOfClusters() throws Exception {
+        return Math.min(m_NumClusters, m_instances.numInstances());
+    }
+
+    //getter
+    //get the random number seed
+    @Override
+    public int getSeed () {
+        return  m_Seed;
+    }
+    
+    // gets the number of clusters
+    public int getNumClusters() {
+        return m_NumClusters;
+    }
+    
+    //setter
+    //set the random number seed
+    @Override
+    public void setSeed (int s) {
+        m_Seed = s;
+    }
+    
+    //set the number of clusters
+    public void setNumClusters(int n) {
+        m_NumClusters = n;
+    }
+
+    //info about the clusters
+    @Override
+    public String toString() {
+        StringBuilder temp = new StringBuilder();
+        temp.append("Within cluster sum of squared errors: "
+        + Utils.sum(m_squaredErrors));
+        temp.append("\nkMeans\n======\n")
+                .append("\nNumber of iterations: ").append(m_Iterations).append("\n")
+                .append("\nCluster members:\n\n");
+        //output instances in all cluster
+        for (int i = 0; i < nClusterID.size(); i++)
+            temp.append("Cluster ").append(i).append(": ").append(nClusterID.get(i).toString()).append("\n");
+        //output all clusters centroid
+        temp.append("\nCluster centroids:\n");
+        for (int i = 0; i < m_NumClusters; i++) {
+            temp.append("\nCluster ").append(i).append(" ");
+            for (int j = 0; j < m_ClusterCentroids.numAttributes(); j++) {
+                if (m_ClusterCentroids.attribute(j).isNominal()) {
+                    temp.append(" ").append(m_ClusterCentroids.attribute(j).
+                            value((int)m_ClusterCentroids.instance(i).value(j)));
+                } else
+                    temp.append(" ").append(m_ClusterCentroids.instance(i).value(j));
+              }
+        }
+        temp.append("\n\n");
+        return temp.toString();
+    }
 }
